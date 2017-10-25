15596b9596a8688df9031a7d09c858ac710024f1
Aung Soe Oo <jealousboy.boy17@gmail.com>
2017-10-25 22:43:42 +0630

    add for in header

--- resources/views/layouts/common/header.blade.php
+++ resources/views/layouts/common/header.blade.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
       <div class="header_bottom">
-        <a href="index.php" class="logo"><img src="{{ asset('images/logo.jpeg') }}" alt="" height="200px" width="1200px"></a>
+        <a href="{{ route('user.index') }}"" class="logo"><img src="{{ asset('images/logo.jpeg') }}" alt="" height="200px" width="1200px"></a>
       </div>
     </div>
   </div>
